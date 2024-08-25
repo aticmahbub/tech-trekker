62286bcfeb1042003b521b077e5d0a235bba9e87
aticcmahbub <aticcmahbub@gmail.com>
2024-08-26 04:16:12 +0600

    project initialized

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NavBar = () => {
+    return (
+        <div className="navbar bg-base-100">
+
+        </div>
+    );
+};
+
+export default NavBar;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+
+const Home = () => {
+    return (
+        <div>
+            {/* nav */}
+            <NavBar/>
+            {/* outlet */}
+            {/* footer */}
+        </div>
+    );
+};
+
+export default Home;
