19523829c37a0312778cad86a845ae8e33d39800
aticcmahbub <aticcmahbub@gmail.com>
2024-08-26 04:17:33 +0600

    navbar added

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,16 @@ import React from 'react';
 const NavBar = () => {
     return (
         <div className="navbar bg-base-100">
-
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">Tech Trekker</a>
+            </div>
+            <div className="flex-none">
+                <ul className="menu menu-horizontal px-1">
+                    <li><a>Home</a></li>
+                    <li><a>Blogs</a></li>
+                    <li><a>Bookmarks</a></li>
+                </ul>
+            </div> 
         </div>
     );
 };
